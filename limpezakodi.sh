f06d3a93e07ee7b6d81c18d57b7be0e271f8d31e
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-08 14:36:46 -0300

    Create limpezakodi.sh

---
+++ limpezakodi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "====================================="
+echo "   SUPER LIMPEZA DO KODI COMUNITY    "
+echo "====================================="
+
+
+
+rm -r /home/kodish/.kodi/addons/temp/*
+rm -r /home/kodish/.kodi/addons/packages/*
+rm -r /home/kodish/.kodi/temp/
+rm -r /home/kodish/.kodi/userdata/Thumbnails/*
