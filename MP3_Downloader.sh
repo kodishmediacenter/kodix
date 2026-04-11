6d36c29606ce40019fb96a94482ccea8099fa012
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-11 15:18:09 -0300

    MP3_Downloader

---
+++ MP3_Downloader.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "====================================="
+echo "   MP3 Downloader                    "
+echo "====================================="
+
+wget https://raw.githubusercontent.com/kodishmediacenter/kodix/refs/heads/main/appimage/MP3_Downloader.AppImage
+chmod +x MP3_Downloader.AppImage
