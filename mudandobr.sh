298a73282f6dad03a5df0a3a4220a501d09cf041
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-14 15:41:18 -0300

    Update mudandobr.sh

--- mudandobr.sh
+++ mudandobr.sh
@@ -6,4 +6,8 @@ echo "====================================="
 
 # Set Mirror para Brasil 
 sudo pacman -S --noconfirm reflector
+
+echo Aplicando configuração para Brasil 
 sudo reflector --country Brazil --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo Atualizando repositorios
+sudo pacman -Syy
