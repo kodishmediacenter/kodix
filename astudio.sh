268954cf4753d587cb30de5dd08fe43a90a29140
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-25 23:09:46 -0300

    Create astudio.sh

---
+++ astudio.sh
@@ -0,0 +1,4 @@
+wget https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.4.6/android-studio-panda4-linux.tar.gz
+tar -xvzf android-studio-panda4-linux.tar.gz
+sudo mv android-studio /opt/
+sh /opt/android-studio/bin/studio.sh
