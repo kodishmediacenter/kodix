61bf55221b49b03741a5ae7b4929b5c055062b0b
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-07-11 12:22:51 -0300

    Create yona.sh

---
+++ yona.sh
@@ -0,0 +1,4 @@
+cd /usr/share/backgrounds/xfce
+sudo rm -r xfce-x.svg
+sudo wget https://raw.githubusercontent.com/kodishmediacenter/Kodish-OS-10/d1b090f5233a7957117fa87fd746ea4bdd2876b3/yona/xfce-x.svg
+echo Download Concluido Reinicie o PC
