3e370544005453760310a393b7b22ccc7ad3373d
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-07-12 13:45:54 -0300

    Create yonasl.sh

---
+++ yonasl.sh
@@ -0,0 +1,4 @@
+cd /usr/share/backgrounds/xfce
+sudo rm -r xfce-x.svg
+sudo wget https://raw.githubusercontent.com/kodishmediacenter/Kodish-OS-10/7c8d3785db8169919f46a166b1f76fc7aaef0cc1/yonasl/xfce-x.svg
+echo Download Concluido Reinicie o PC
