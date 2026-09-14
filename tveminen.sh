8a32fa7c92b9756072144d63ca7d704f626615a6
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-09-14 18:22:15 -0300

    Update tveminen.sh

--- tveminen.sh
+++ tveminen.sh
@@ -3,6 +3,7 @@
 flatpak install flathub tv.kodi.Kodi
 cd $HOME/.var/app/tv.kodi.Kodi
 mv data data2
+cd data
 wget https://archive.org/download/tv-eminem-3.0.1-32/TV-EMINEM-3.0.1-32.zip
 rm -r *.zip
 mv TV-EMINEM-3.0.1-32.zip data.zip
